2d7178b0445a9e09b9fd3bcee4bfa4e865eb3b29
YangTianqi <1211325315@qq.com>
2022-11-27 20:12:31 +0800

    add entity @s

--- Project/src/main/java/entity/IssueCase.java
+++ Project/src/main/java/entity/IssueCase.java
@@ -2,11 +2,15 @@ package entity;
 
 import common.CASE_STATUS;
 import common.CASE_TYPE;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class IssueCase {
     Integer issue_case_id;
     Integer appear_commit_id;
--- Project/src/main/java/entity/Repository.java
+++ Project/src/main/java/entity/Repository.java
@@ -1,8 +1,12 @@
 package entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Repository {
     Integer repository_id ;
     String name;
