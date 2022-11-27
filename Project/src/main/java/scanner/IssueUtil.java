0cf79edb30b3916975aef20534f58a3173f7e9e0
YangTianqi <1211325315@qq.com>
2022-11-27 21:11:14 +0800

    update IssueInstance

--- Project/src/main/java/scanner/IssueUtil.java
+++ Project/src/main/java/scanner/IssueUtil.java
@@ -4,6 +4,7 @@ import cn.edu.fudan.issue.entity.dbo.Location;
 import cn.edu.fudan.issue.entity.dbo.RawIssue;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import entity.IssueLocation;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -53,12 +54,13 @@ public class IssueUtil {
         //获取issue数量
         try {
             JSONArray sonarRawIssues = getSonarIssueResults(repoUuid + "_" + commit).getJSONArray("issues");
+            System.out.println(sonarRawIssues);
             //解析sonar的issues为平台的rawIssue
             for (int j = 0; j < sonarRawIssues.size(); j++) {
                 JSONObject sonarIssue = sonarRawIssues.getJSONObject(j);
 
                 //解析location
-                List<Location> locations = getLocations(sonarIssue, repoPath);
+                List<IssueLocation> locations = getLocations(sonarIssue, repoPath);
                 if (locations.isEmpty()) {
                     continue;
                 }
@@ -78,43 +80,41 @@ public class IssueUtil {
         }
     }
 
-    public List<Location> getLocations(JSONObject issue, String repoPath) throws Exception {
+    public static List<IssueLocation> getLocations(JSONObject issue, String repoPath) throws Exception {
         int startLine = 0;
         int endLine = 0;
+        String filePath = null;
+
         String sonarPath;
         String[] sonarComponents;
-        String filePath = null;
-        List<Location> locations = new ArrayList<>();
+        List<IssueLocation> locations = new ArrayList<>();
+        int sequence = 1;
         JSONArray flows = issue.getJSONArray("flows");
         if (flows.size() == 0) {
-            //第一种针对issue中的textRange存储location
             JSONObject textRange = issue.getJSONObject("textRange");
             if (textRange != null) {
                 startLine = textRange.getIntValue("startLine");
                 endLine = textRange.getIntValue("endLine");
             } else {
-                // 无 location 行号信息的 issue 过滤掉
                 return new ArrayList<>();
             }
-
-            sonarPath = issue.getString("component");
-            if (sonarPath != null) {
-                sonarComponents = sonarPath.split(":");
-                if (sonarComponents.length >= 2) {
-                    filePath = sonarComponents[sonarComponents.length - 1];
-                }
-            }
-
-            Location mainLocation = getLocation(startLine, endLine, filePath, repoPath);
+//            sonarPath = issue.getString("component");
+//            if (sonarPath != null) {
+//                sonarComponents = sonarPath.split(":");
+//                if (sonarComponents.length >= 2) {
+//                    filePath = sonarComponents[sonarComponents.length - 1];
+//                }
+//            }
+            IssueLocation mainLocation = new IssueLocation(sequence, startLine, endLine);
             locations.add(mainLocation);
-        } else {
-            //第二种针对issue中的flows中的所有location存储
+        }
+        else {
             for (int i = 0; i < flows.size(); i++) {
                 JSONObject flow = flows.getJSONObject(i);
                 JSONArray flowLocations = flow.getJSONArray("locations");
-                //一个flows里面有多个locations， locations是一个数组，目前看sonar的结果每个locations都是一个location，但是不排除有多个。
                 for (int j = 0; j < flowLocations.size(); j++) {
                     JSONObject flowLocation = flowLocations.getJSONObject(j);
+
                     String flowComponent = flowLocation.getString("component");
                     JSONObject flowTextRange = flowLocation.getJSONObject("textRange");
                     if (flowTextRange == null || flowComponent == null) {
@@ -122,38 +122,31 @@ public class IssueUtil {
                     }
                     int flowStartLine = flowTextRange.getIntValue("startLine");
                     int flowEndLine = flowTextRange.getIntValue("endLine");
-                    String flowFilePath = null;
-
-                    String[] flowComponents = flowComponent.split(":");
-                    if (flowComponents.length >= 2) {
-                        flowFilePath = flowComponents[flowComponents.length - 1];
-                    }
-
-                    Location location = getLocation(flowStartLine, flowEndLine, flowFilePath, repoPath);
+                    IssueLocation location = new IssueLocation(sequence, flowStartLine, flowEndLine);
+                    sequence++;
                     locations.add(location);
                 }
             }
         }
-
         return locations;
     }
 
-    static Location getLocation(int startLine, int endLine, String filePath, String repoPath){
-        Location location = new Location();
-        location.setStartLine(startLine);
-        location.setEndLine(endLine);
-        location.setStartToken(0);
-        location.setFilePath(filePath);
-        return location;
-    }
-
-    static RawIssue getRawIssue(String repoUuid, String commit, String type, String repoPath){
-        RawIssue rawIssue = new RawIssue();
-        rawIssue.setUuid(repoUuid);
-        rawIssue.setType(type);
-        rawIssue.setFileName("src/main/resources/testFile/commit1/MatchFailed.java");
-        rawIssue.setDetail("Cast one of the operands of this multiplication operation to a \"long\".---MINOR");
-        rawIssue.setCommitId("commit1");
-        return rawIssue;
-    }
+//    static Location getLocation(int startLine, int endLine, String filePath, String repoPath){
+//        Location location = new Location();
+//        location.setStartLine(startLine);
+//        location.setEndLine(endLine);
+//        location.setStartToken(0);
+//        location.setFilePath(filePath);
+//        return location;
+//    }
+//
+//    static RawIssue getRawIssue(String repoUuid, String commit, String type, String repoPath){
+//        RawIssue rawIssue = new RawIssue();
+//        rawIssue.setUuid(repoUuid);
+//        rawIssue.setType(type);
+//        rawIssue.setFileName("src/main/resources/testFile/commit1/MatchFailed.java");
+//        rawIssue.setDetail("Cast one of the operands of this multiplication operation to a \"long\".---MINOR");
+//        rawIssue.setCommitId("commit1");
+//        return rawIssue;
+//    }
 }
