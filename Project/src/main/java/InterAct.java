77d1175340258beb4f3f12ad09daeeb7635e925a
zhangjiaxun <20302010032@fudan.edu.cn>
2022-11-27 19:25:14 +0800

    完善交互框架

--- Project/src/main/java/InterAct.java
+++ Project/src/main/java/InterAct.java
@@ -1,6 +1,39 @@
+import java.util.Scanner;
+
 public class InterAct {
     public static void InterActWithUserCommand(){
+        boolean exitFlag = false;
+        Scanner s = new Scanner(System.in);
+        while(!exitFlag){
+            System.out.println("请输入指令：                 -------(输入help查看所有指令)");
+            String command = s.nextLine();
+            switch (command){
+                case "help":
+                    printHelp();
+                    break;
+                case "commit":
+                    break;
+                case "time":
+                    break;
+                case "committer":
+                    break;
+                case "exit":
+                    exitFlag = true;
+                    break;
+                default:
+                    System.out.println("错误的指令，输入help查看所有指令\n\n");
+            }
+        }
 
     }
+
+    private static void printHelp(){
+        System.out.println("--help   查看所有指令");
+        System.out.println("--commit 查看指定版本");
+        System.out.println("--time   查看指定时间段");
+        System.out.println("--committer 查看指定程序员");
+        System.out.println("--exit 退出");
+    }
+
 
 }
